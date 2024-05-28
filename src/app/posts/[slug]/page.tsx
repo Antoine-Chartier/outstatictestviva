a074e65ae57e9ac17888446c602db30323f90b70
win116 <antoinechartier.ca@gmail.com>
2024-05-27 20:39:57 -0400

    Refactor getStaticPaths function to return a Promise in page.tsx

--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -132,13 +132,14 @@ async function getData({ params }: Params) {
 
 // This function gets called at build time on server-side. It won't be called on client-side.
 // It allows you to fetch data and pre-render the page. This is useful for SEO purposes.
-export async function getStaticPaths() {
+export async function getStaticPaths(): Promise<{paths: {params: {slug: string}}[], fallback: boolean}> {
   const paths = await generateStaticParams();
   return {
     paths,
     fallback: false,
   };
 }
+
 export async function generateStaticParams() {
   const posts = getDocumentSlugs('posts')
   return posts.map((slug) => ({ params: { slug } }))
