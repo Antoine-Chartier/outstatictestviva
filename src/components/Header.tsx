011fbbc9a676e6ff182bbe163807aee775bdacd1
Antoine Chartier <121460528+Antoine-Chartier@users.noreply.github.com>
2026-09-13 18:11:25 +0000

    test

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,38 +1,23 @@
-import Link from 'next/link'
-import { getDocumentSlugs } from 'outstatic/server'
-import { load } from 'outstatic/server'
 import Image from 'next/image'
-
-
-
+import Link from 'next/link'
+import { load, getDocumentSlugs } from 'outstatic/server'
 
 const Header = async () => {
-
-const pagesSlug = await generateStaticParams()
-// console.log(pagesSlug);
-
+  const pagesSlug = await generateStaticParams()
+  
   return (
     <nav className="layout flex items-center justify-between py-4">
       <div>
         <Link href="/" className="hover:underline">
-      <Image
-        src="images/logos/logo.png"
-        alt="Logo"
-        width={100}
-        height={100}
-        priority={true}
-      />
+          <Image
+            src="/images/logos/logo.png"
+            alt="Logo"
+            width={100}
+            height={100}
+            priority={true}
+          />
         </Link>
       </div>
-      <ul className="flex items-center justify-between space-x-3 text-xs md:space-x-4 md:text-base">
-        {pagesSlug.map(({slug}) => (
-          <li key={slug}>
-            <Link href={`/${slug}`} className="hover:underline">
-              {slug} 
-            </Link>
-          </li>
-        ))}
-      </ul>
     </nav>
   )
 }
